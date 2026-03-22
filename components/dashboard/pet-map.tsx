aa10685afcc125ac49452127dbc9eabd1cab7305
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2026-03-22 05:15:55 -0300

    a ver..

--- components/dashboard/pet-map.tsx
+++ components/dashboard/pet-map.tsx
@@ -154,7 +154,7 @@ export function PetMap({ scans, events }: PetMapProps) {
       {/* Mapa expandido (modal) */}
       {expanded && (
         <div
-          className="fixed inset-0 bg-black/60 z-50 flex items-center justify-center p-4"
+        className="fixed inset-0 bg-black/60 z-[9999] flex items-center justify-center p-4"
           onClick={() => setExpanded(false)}
         >
           <div
